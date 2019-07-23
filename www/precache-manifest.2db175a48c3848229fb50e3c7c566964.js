897a7cc72f33e154a8e8b4d6e576dbe7cd36095d
nelsondevweb <butarbutarn1981@gmail.com>
2019-07-23 20:34:48 +0700

    Initialize app first

---
+++ www/precache-manifest.2db175a48c3848229fb50e3c7c566964.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68ea51ba02e27800ec6c7b16ca8457f8",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "05efa2138787fa4233d5",
+    "url": "/calculator/static/css/main.816a743a.chunk.css"
+  },
+  {
+    "revision": "d77ad4bf4a1d2d5db2f0",
+    "url": "/calculator/static/js/2.8b196a34.chunk.js"
+  },
+  {
+    "revision": "05efa2138787fa4233d5",
+    "url": "/calculator/static/js/main.17766971.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
